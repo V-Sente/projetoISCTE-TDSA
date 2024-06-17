bbb43343159d1c899389598bc9002fc1a86b6877
V-Sente <vicentepacheco04@gmail.com>
2024-06-17 12:16:03 +0100

    .

--- app/index/forms.py
+++ app/index/forms.py
@@ -1,25 +1,20 @@
+# forms.py
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth.models import User
-
+from django.contrib.auth.forms import UserCreationForm
+from .models import CustomUser
 
 class CreateUserForm(UserCreationForm):
-    nome = forms.CharField(widget=forms.Textarea())  # ALTERANDO O TAMANHO DA CAIXA DE TEXTO
-
     class Meta:
-        model = User
-        fields = ['nome', 'email', 'data_nascimento', 'sexo', 'n_telemovel', 'password1', 'password2']
+        model = CustomUser
+        fields = ['username', 'data_nascimento', 'sexo', 'email', 'n_telemovel', 'password1', 'password2']
         widgets = {
             'data_nascimento': forms.DateInput(attrs={'type': 'date'}),
         }
 
-
 class CreateCreatorForm(UserCreationForm):
-    nome = forms.CharField(widget=forms.Textarea())  # ALTERANDO O TAMANHO DA CAIXA DE TEXTO
-
     class Meta:
-        model = User
-        fields = ['nome', 'email', 'data_nascimento', 'sexo', 'n_telemovel', 'organizacao', 'cargo', 'password1', 'password2']
+        model = CustomUser
+        fields = ['email_organizacao', 'username', 'organizacao', 'cargo']
         widgets = {
             'data_nascimento': forms.DateInput(attrs={'type': 'date'}),
         }
--- app/index/models.py
+++ app/index/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    data_nascimento = models.DateField(null=True, blank=True)
+    sexo = models.CharField(max_length=10, null=True, blank=True)
+    n_telemovel = models.CharField(max_length=15, null=True, blank=True)
+    email_organizacao = models.EmailField(max_length=100, null=True, blank=True)
+    organizacao = models.CharField(max_length=100, null=True, blank=True)
+    cargo = models.CharField(max_length=100, null=True, blank=True)
+
+    groups = models.ManyToManyField(Group, related_name='customuser_set', blank=True)
+    user_permissions = models.ManyToManyField(Permission, related_name='customuser_set', blank=True)
+
+    def __str__(self):
+        return self.username
